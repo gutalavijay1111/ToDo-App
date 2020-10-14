e2468f28b60f7a112265022ba70c299c185b2df7
gutalavijay1111 <gutalavijay1111kumar@gmail.com>
2020-10-15 03:41:45 +0530

    Made UI Changes

--- todo/forms.py
+++ todo/forms.py
@@ -3,4 +3,9 @@ from django import forms
 class TodoForm(forms.Form):
     text = forms.CharField(max_length=40, 
         widget=forms.TextInput(
-            attrs={'class' : 'form-control', 'placeholder' : 'Enter todo e.g. Delete junk files', 'aria-label' : 'Todo', 'aria-describedby' : 'add-btn'}))
+            attrs = {
+                'class' : 'form-control',
+                'placeholder' : 'I\'ll do This today ',
+                'aria-label' : 'Todo',
+                'aria-describedby' : 'add-btn'
+            }))
