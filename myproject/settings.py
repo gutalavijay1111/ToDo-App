d1e0de449dfe2a9337cace12367f949b977fc982
gutalavijay1111 <gutalavijay1111kumar@gmail.com>
2020-10-19 00:27:14 +0530

    static

--- myproject/settings.py
+++ myproject/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/todo'),
+    os.path.join(BASE_DIR, '/static/'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
